447e64d1f13b691170784d80a9aaef419bc0e7fe
tobyteel <tobyteel@tobyteel.com>
2013-11-27 22:43:17 -0800

    Made GA track page changes

--- includes/ga_tracking.js
+++ includes/ga_tracking.js
@@ -1,8 +1,15 @@
 var _gaq = _gaq || [];
 _gaq.push(['_setAccount', 'UA-3825515-1']);
-_gaq.push(['_trackPageview']);
 (function() {
 var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
 ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
 var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
+
+$(document).on("pageshow", "[data-role=page]", function (event, el) {
+	if ($.mobile.activePage.attr("data-url")) {
+        _gaq.push(['_trackPageview', $.mobile.activePage.attr("data-url")]);
+    } else {
+        _gaq.push(['_trackPageview']);
+    }
+});
